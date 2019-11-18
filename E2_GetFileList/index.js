bf65d706a3a7ca98922d67def6ed017d8ff9ad31
Kohei Akiyama <akiyamak@isao.co.jp>
2019-11-18 09:22:18 +0900

    add fan-in-out sample https://docs.microsoft.com/en-us/azure/azure-functions/durable/durable-functions-cloud-backup

---
+++ E2_GetFileList/index.js
@@ -0,0 +1,13 @@
+﻿const readdirp = require("readdirp");
+
+module.exports = async function (context, rootDirectory) {
+    context.log(`Searching for files under '${rootDirectory}'...`);
+    const allFilePaths = [];
+    for await (const entry of readdirp(rootDirectory, { type: 'all' })) {
+        if (!entry.dirent.isDirectory()) {
+            allFilePaths.push(entry.fullPath);
+        }
+    }
+    context.log(`Found ${allFilePaths.length} under ${rootDirectory}.`);
+    context.done(null, allFilePaths);
+};
